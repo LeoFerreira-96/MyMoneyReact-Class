e0e72401be4d233be07c7e57d8f039746d3c1aa9
LeoFerreira-96 <leonardo.ferreira96@outlook.com.br>
2020-04-01 16:38:26 -0300

    criando o mapeamento de rotas

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb+srv://leoguime:123mudar@cluster0-d8yjs.mongodb.net/test?retryWrites=true&w=majority',{})
+module.exports = mongoose.connect('mongodb+srv://leoguime:123mudar@cluster0-d8yjs.mongodb.net/test?retryWrites=true&w=majority', {})
