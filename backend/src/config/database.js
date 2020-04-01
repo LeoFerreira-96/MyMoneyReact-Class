d8af39d8a10dbc30cc260e5f62110d866d3529bc
LeoFerreira-96 <leonardo.ferreira96@outlook.com.br>
2020-04-01 16:54:32 -0300

    configurando mensagens de erro

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb+srv://leoguime:123mudar@cluster0-d8yjs.mongodb.net/test?retryWrites=true&w=majority', {})
+
+mongoose.Error.messages.general.required = "O atributo '{PAT}' é obrigatorio."
+mongoose.Error.messages.Number.min = "O '{VALUE}' informado é menor que o limite minimo de '{MIN}'."
+mongoose.Error.messages.Number.max = "O '{VALUE}' informado é maior que o limite minimo de '{MAX}'."
+mongoose.Error.messages.Number.min = "'{VALUE}' não é válido para o atributo '{PATH}'."
