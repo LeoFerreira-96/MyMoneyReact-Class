59e334890517e6a5c58d49285a425db2d43a4277
LeoFerreira-96 <leonardo.ferreira96@outlook.com.br>
2020-04-05 23:15:57 -0300

    Criado a navegação com react-router

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,13 +4,15 @@ import '../common/template/dependecies'
 import Header from '../common/template/header' 
 import SideBar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
+import Routes from './routes'
+import { Router } from 'react-router'
 
 export default props => (
     <div className="wrapper">
         <Header />
         <SideBar />
         <div className='content-wrapper'>
-            <h1>Conteudo</h1>
+            <Routes />
         </div>
         <Footer />
     </div>
