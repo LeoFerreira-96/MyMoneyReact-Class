9696a7701eaff83037d9ffbf2ed734a69fbbe089
LeoFerreira-96 <leonardo.ferreira96@outlook.com.br>
2020-04-06 13:28:47 -0300

    criado componente Grid

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,14 +1,13 @@
 import React, {Component }from 'react'
 
-import Component from '../common/template/contentHeader'
+import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
-import contentHeader from '../common/template/contentHeader'
 
 class Dashboard extends Component {
     render() {
         return (
             <div>
-                <contentHeader title='Dashboard' small='Versão 1.0' />
+                <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
                     Dashboard
                 </Content>
