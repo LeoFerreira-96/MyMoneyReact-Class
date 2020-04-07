33d33bef0bdf1af648ba6b0a1a02160b45f2410d
LeoFerreira-96 <leonardo.ferreira96@outlook.com.br>
2020-04-07 17:45:04 -0300

    Criando Componentes Estruturais das abas de Navegação

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { bindActionCreators} from 'redux'
+import { bindActionCreators } from 'redux'
 
-import {getSummary} from './dashboardAction'
+import { getSummary } from './dashboardAction'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
@@ -10,21 +10,21 @@ import Row from '../common/layout/row'
 
 class Dashboard extends Component {
 
-    componentWillMount(){
+    componentWillMount() {
         this.props.getSummary()
     }
 
     render() {
-        const {credit = 0, debt = 0 } = this.props.summary
-        
+        const { credit, debt } = this.props.summary
+
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
                     <Row>
-                        <ValueBox cols='12 4' color='green' icon='bank' value={`${credit.toLocaleString('pt-BR', { minimumFractionDigits: 2 , style: 'currency', currency: 'BRL' })}`} text='Total de Receita' />
-                        <ValueBox cols='12 4' color='red' icon='credit-card' value={` ${debt.toLocaleString('pt-BR', { minimumFractionDigits: 2 , style: 'currency', currency: 'BRL' })}`} text='Total de Despesas' />
-                        <ValueBox cols='12 4' color='blue' icon='money' value={`${(credit - debt).toLocaleString('pt-BR', { minimumFractionDigits: 2 , style: 'currency', currency: 'BRL' })}`} text='Valor Consolidado' />
+                        <ValueBox cols='12 4' color='green' icon='bank' value={`${credit.toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })}`} text='Total de Receita' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value={` ${debt.toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })}`} text='Total de Despesas' />
+                        <ValueBox cols='12 4' color='blue' icon='money' value={`${(credit - debt).toLocaleString('pt-BR', { minimumFractionDigits: 2, style: 'currency', currency: 'BRL' })}`} text='Valor Consolidado' />
                     </Row>
                 </Content>
             </div>
@@ -32,6 +32,6 @@ class Dashboard extends Component {
     }
 }
 
-const mapStateToProps = state => ({summary: state.dashboard.summary})
-const mapDispathToPorps = dispatch => bindActionCreators({getSummary}, dispatch)
+const mapStateToProps = state => ({ summary: state.dashboard.summary })
+const mapDispathToPorps = dispatch => bindActionCreators({ getSummary }, dispatch)
 export default connect(mapStateToProps, mapDispathToPorps)(Dashboard)
