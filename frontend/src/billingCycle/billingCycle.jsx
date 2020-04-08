4cb0aafcb6fd4e48f4304241f66dc7fbc49c931d
LeoFerreira-96 <leonardo.ferreira96@outlook.com.br>
2020-04-08 15:49:17 -0300

    Visibilidade das abas

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import Tabs from '../common/tab/tabs'
@@ -6,9 +9,15 @@ import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
-import tabContent from '../common/tab/tabContent'
+import {selectTab, showTabs} from '../common/tab/tabActions'
+
 
 class BillingCycle extends Component {
+componentWillMount(){
+    this.props.selectTab('tabList')
+    this.props.showTabs('tabList','tabCreate')
+}
+
     render() {
         return (
             <div>
@@ -34,4 +43,6 @@ class BillingCycle extends Component {
     }
 }
 
-export default BillingCycle
+const mapDipatchToProps = dispatch => bindActionCreators({selectTab, showTabs},dispatch)
+
+export default connect (null, mapDipatchToProps)(BillingCycle)
