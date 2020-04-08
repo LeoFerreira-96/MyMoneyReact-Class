09a99bf1b200f2b23fd500aa6e10fdc2d1213010
LeoFerreira-96 <leonardo.ferreira96@outlook.com.br>
2020-04-08 16:44:37 -0300

    criado componente BillingCycleList

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import {bindActionCreators} from 'redux'
-import {connect} from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -9,14 +9,15 @@ import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
-import {selectTab, showTabs} from '../common/tab/tabActions'
+import { selectTab, showTabs } from '../common/tab/tabActions'
+import List from './billingCycleList'
 
 
 class BillingCycle extends Component {
-componentWillMount(){
-    this.props.selectTab('tabList')
-    this.props.showTabs('tabList','tabCreate')
-}
+    componentWillMount() {
+        this.props.selectTab('tabList')
+        this.props.showTabs('tabList', 'tabCreate')
+    }
 
     render() {
         return (
@@ -31,7 +32,9 @@ componentWillMount(){
                             <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
                         </TabsHeader>
                         <TabsContent >
-                            <TabContent id='tabList'><h1>Listar</h1></TabContent>
+                            <TabContent id='tabList'>
+                                <List />
+                            </TabContent>
                             <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
                             <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
                             <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
@@ -43,6 +46,6 @@ componentWillMount(){
     }
 }
 
-const mapDipatchToProps = dispatch => bindActionCreators({selectTab, showTabs},dispatch)
+const mapDipatchToProps = dispatch => bindActionCreators({ selectTab, showTabs }, dispatch)
 
-export default connect (null, mapDipatchToProps)(BillingCycle)
+export default connect(null, mapDipatchToProps)(BillingCycle)
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -2,10 +2,13 @@ import { combineReducers } from 'redux'
 
 import dashboardReducer from '../dashboard/dashboardReducer'
 import tabReducer from '../common/tab/tabReducer'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
+import billingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducer = combineReducers({
     dashboard: dashboardReducer,
-    tab: tabReducer
+    tab: tabReducer,
+    billingCycle: billingCycleReducer
 })
 
 export default rootReducer
