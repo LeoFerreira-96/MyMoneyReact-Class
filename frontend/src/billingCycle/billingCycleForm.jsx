f4c7f812ed27960d8061845be84506c3f760859a
LeoFerreira-96 <leonardo.ferreira96@outlook.com.br>
2020-04-13 14:36:55 -0300

    melhorado componente de form

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
+import labelAndInput from '../common/form/labelInput'
 
 class BillingCycleForm extends Component {
 
@@ -8,9 +9,9 @@ class BillingCycleForm extends Component {
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className='box-body'>
-                    <Field name='name' component='input'/>
-                    <Field name='month' component='input'/>
-                    <Field name='year' component='input'/>
+                    <Field name='name' component={labelAndInput} label='Nome' cols='12 4' placeholder='Informe o nome' readOnly='' type='' />
+                    <Field name='month' component={labelAndInput} label='Mês' cols='12 4' placeholder='Informe o mês' readOnly='' type='' />
+                    <Field name='year' component={labelAndInput} label='Ano' cols='12 4' placeholder='Informe o ano' readOnly='' type='' />
                 </div>
                 <div className='box-footer'>
                     <button type='submit' className='btn btn-primary'>Submit</button>
