3d6994fb8c3e7fd0299fc8283876783973563dfd
LeoFerreira-96 <leonardo.ferreira96@outlook.com.br>
2020-06-09 22:40:27 -0300

    Criado Botao de excluir e metodos de exclusão

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,58 +1,60 @@
-import React, { Component } from 'react'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
-import ContentHeader from '../common/template/contentHeader'
-import Content from '../common/template/content'
-import Tabs from '../common/tab/tabs'
-import TabsHeader from '../common/tab/tabsHeader'
-import TabsContent from '../common/tab/tabsContent'
-import TabHeader from '../common/tab/tabHeader'
-import TabContent from '../common/tab/tabContent'
-import { selectTab, showTabs } from '../common/tab/tabActions'
-import { create, update } from '../billingCycle/billingCyclesAction'
+import ContentHeader from "../common/template/contentHeader";
+import Content from "../common/template/content";
+import Tabs from "../common/tab/tabs";
+import TabsHeader from "../common/tab/tabsHeader";
+import TabsContent from "../common/tab/tabsContent";
+import TabHeader from "../common/tab/tabHeader";
+import TabContent from "../common/tab/tabContent";
+import { selectTab, showTabs } from "../common/tab/tabActions";
+import { create, update, remove } from "../billingCycle/billingCyclesAction";
 
-import List from './billingCycleList'
-import Form from './billingCycleForm'
-
+import List from "./billingCycleList";
+import Form from "./billingCycleForm";
 
 class BillingCycle extends Component {
-    componentWillMount() {
-        this.props.selectTab('tabList')
-        this.props.showTabs('tabList', 'tabCreate')
-    }
+  componentWillMount() {
+    this.props.selectTab("tabList");
+    this.props.showTabs("tabList", "tabCreate");
+  }
 
-    render() {
-        return (
-            <div>
-                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
-                <Content>
-                    <Tabs>
-                        <TabsHeader>
-                            <TabHeader label='Listar' icon='bars' target='tabList' />
-                            <TabHeader label='Incluir' icon='plus' target='tabCreate' />
-                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
-                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete' />
-                        </TabsHeader>
-                        <TabsContent >
-                            <TabContent id='tabList'>
-                                <List />
-                            </TabContent>
-                            <TabContent id='tabCreate'>
-                                <Form onSubmit={this.props.create} />
-                            </TabContent>
-                            <TabContent id='tabUpdate'>
-                                <Form onSubmit={this.props.update} />
-                            </TabContent>
-                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
-                        </TabsContent>
-                    </Tabs>
-                </Content>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <ContentHeader title="Ciclos de Pagamentos" small="Cadastro" />
+        <Content>
+          <Tabs>
+            <TabsHeader>
+              <TabHeader label="Listar" icon="bars" target="tabList" />
+              <TabHeader label="Incluir" icon="plus" target="tabCreate" />
+              <TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
+              <TabHeader label="Excluir" icon="trash-o" target="tabDelete" />
+            </TabsHeader>
+            <TabsContent>
+              <TabContent id="tabList">
+                <List />
+              </TabContent>
+              <TabContent id="tabCreate">
+                <Form onSubmit={this.props.create} />
+              </TabContent>
+              <TabContent id="tabUpdate">
+                <Form onSubmit={this.props.update} />
+              </TabContent>
+              <TabContent id="tabDelete">
+                <Form onSubmit={this.props.remove} readOnly={true} />
+              </TabContent>
+            </TabsContent>
+          </Tabs>
+        </Content>
+      </div>
+    );
+  }
 }
 
-const mapDipatchToProps = dispatch => bindActionCreators({ create, update, selectTab, showTabs }, dispatch)
+const mapDipatchToProps = (dispatch) =>
+  bindActionCreators({ create, update, remove, selectTab, showTabs }, dispatch);
 
-export default connect(null, mapDipatchToProps)(BillingCycle)
+export default connect(null, mapDipatchToProps)(BillingCycle);
--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,31 +1,65 @@
-import React, { Component } from 'react'
-import {connect} from 'react-redux'
-import { bindActionCreators} from 'redux'
-import { reduxForm, Field} from 'redux-form'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { reduxForm, Field } from "redux-form";
 
-import {init} from '../billingCycle/billingCyclesAction'
-import labelAndInput from '../common/form/labelInput'
+import { init } from "../billingCycle/billingCyclesAction";
+import labelAndInput from "../common/form/labelInput";
 
 class BillingCycleForm extends Component {
-
-    render() {
-        const { handleSubmit } = this.props
-        return (
-            <form role='form' onSubmit={handleSubmit}>
-                <div className='box-body'>
-                    <Field name='name' component={labelAndInput} label='Nome' cols='12 4' placeholder='Informe o nome' readOnly='' type='' />
-                    <Field name='month' component={labelAndInput} label='Mês' cols='12 4' placeholder='Informe o mês' readOnly='' type='' />
-                    <Field name='year' component={labelAndInput} label='Ano' cols='12 4' placeholder='Informe o ano' readOnly='' type='' />
-                </div>
-                <div className='box-footer'>
-                    <button type='submit' className='btn btn-primary'>Submit</button>
-                    <button type='button' className='btn btn-default' onClick={this.props.init}>Cancelar</button>
-                </div>
-            </form>
-        )
-    }
+  render() {
+    const { handleSubmit, readOnly } = this.props;
+    return (
+      <form role="form" onSubmit={handleSubmit}>
+        <div className="box-body">
+          <Field
+            name="name"
+            component={labelAndInput}
+            label="Nome"
+            cols="12 4"
+            placeholder="Informe o nome"
+            readOnly={readOnly}
+            type=""
+          />
+          <Field
+            name="month"
+            component={labelAndInput}
+            label="Mês"
+            cols="12 4"
+            placeholder="Informe o mês"
+            readOnly={readOnly}
+            type=""
+          />
+          <Field
+            name="year"
+            component={labelAndInput}
+            label="Ano"
+            cols="12 4"
+            placeholder="Informe o ano"
+            readOnly={readOnly}
+            type=""
+          />
+        </div>
+        <div className="box-footer">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+          <button
+            type="button"
+            className="btn btn-default"
+            onClick={this.props.init}
+          >
+            Cancelar
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
 
-BillingCycleForm = reduxForm({ form: 'billingCycleForm', destroyOnUnmount: false })(BillingCycleForm)
-const mapDispatchToProps = dispatch => bindActionCreators({init},dispatch)
-export default connect(null, mapDispatchToProps)(BillingCycleForm)
+BillingCycleForm = reduxForm({
+  form: "billingCycleForm",
+  destroyOnUnmount: false,
+})(BillingCycleForm);
+const mapDispatchToProps = (dispatch) => bindActionCreators({ init }, dispatch);
+export default connect(null, mapDispatchToProps)(BillingCycleForm);
